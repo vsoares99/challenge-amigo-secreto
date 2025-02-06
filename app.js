d36da01b6ba8011b50afe03d8127cf531666f1e6
vsoares99 <victor.o.soares@hotmail.com>
2025-02-06 07:36:46 -0300

    Implementar função para exibir amigos na tela e função para sortear amigo

--- app.js
+++ app.js
@@ -4,19 +4,50 @@ function adicionarAmigo(){
     let nomeAmigo = document.querySelector('input').value;
 
     if(nomeAmigo == ''){
-        alert("Por favor, insira um nome.");
+        alert('Por favor, insira um nome.');
     }
     else if(amigos.includes(nomeAmigo)){
-        alert("Não pode repetir o mesmo nome");
+        alert(`Você já adicionou o nome ${nomeAmigo}. Em caso de amigos com mesmo nome, adicione o sobrenome ou coloque o apelido.`);
     }
     else{
         amigos.push(nomeAmigo);
     }
-    console.log(amigos);
     limparCampo();
+    exibirAmigos();
+}
+
+function exibirAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    let i = 0;
+    let contarAmigos = 0;
+
+    while(i < amigos.length){
+        let itemLista = document.createElement('li');
+        itemLista.textContent = amigos[i];
+        listaAmigos.appendChild(itemLista);
+        i++;
+        contarAmigos++;
+    }
+    let palavraAmigo = contarAmigos > 1 ? 'Amigos' : 'Amigo';
+    let campo = document.getElementById('contarAmigos');
+    campo.innerHTML = `${contarAmigos + ' ' + palavraAmigo}`;
 }
 
 function limparCampo(){
     nomeAmigo = document.querySelector('input');
     nomeAmigo.value = '';
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0){
+        document.getElementById('resultado').innerHTML = 'Não há amigos disponíveis para sortear.';
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById('resultado').innerHTML = `<li>Amigo sorteado é: ${amigoSorteado}</li>`;
 }
